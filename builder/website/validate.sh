94271fa21ac01b583257f2cdff5ba3d9520d206a
Richard W.M. Jones <rjones@redhat.com>
2013-11-05 07:25:46 +0000

    builder/website: Add index validation test script.
    (cherry picked from commit 154bfb7dd46e7998eed1d40ead95923590763739)

--- builder/website/validate.sh
+++ builder/website/validate.sh
@@ -1,4 +1,5 @@
-# libguestfs virt-builder tool
+#!/bin/bash -
+# libguestfs virt-builder validate index
 # Copyright (C) 2013 Red Hat Inc.
 #
 # This program is free software; you can redistribute it and/or modify
@@ -15,35 +16,9 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
-include $(top_srcdir)/subdir-rules.mk
+export LANG=C
+set -e
 
-EXTRA_DIST = \
-	.gitignore \
-	compress.sh \
-	test-guest.sh \
-	README \
-	index \
-	index.asc \
-	centos.sh \
-	centos-6.xz.sig \
-	debian.preseed \
-	debian.sh \
-	debian-6.xz.sig \
-	debian-7.xz.sig \
-	fedora.sh \
-	fedora-18.xz.sig \
-	fedora-19.xz.sig \
-	rhel.sh \
-	scientificlinux.sh \
-	scientificlinux-6.xz.sig \
-	ubuntu.preseed \
-	ubuntu.sh \
-	ubuntu-10.04.xz.sig \
-	ubuntu-12.04.xz.sig \
-	ubuntu-13.10.xz.sig
+../virt-index-validate index
+../virt-index-validate index.asc
 
-CLEANFILES = *~
-
-# Validates the index file.
-TESTS_ENVIRONMENT = $(top_builddir)/run --test
-TESTS = validate.sh
